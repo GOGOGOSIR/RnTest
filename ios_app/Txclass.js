12c7002c66992c05861ef5f21a1c32b4656096ec
wanjianqiang <17688728066@163.com>
2020-11-20 19:20:34 +0800

    tab-navigation test

--- ios_app/Txclass.js
+++ ios_app/Txclass.js
@@ -1,22 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
-// import Content from './components/Content/index';
+import { StyleSheet, Image, Button } from 'react-native';
 import IndexModel from './models/index';
 import { screenSize } from './utils/tools';
 // Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+// icon
+// import { Ionicons } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// 页面
 import ListPage from './pages/list';
 import HomePage from './pages/home';
 import DetailPage from './pages/detail';
 import ModalScreen from './pages/ModalScreen';
+import MyPage from './pages/my';
 
 const indexModel = new IndexModel();
 const { screenWidth, screenHeight } = screenSize;
 
 const MainStack = createStackNavigator();
 const RootStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+// fix: Ionicons Unrecognized font family
+Ionicons.loadFont();
+
+
+function TabStackScreen() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+          } else if (route.name === 'My') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          headerTitle: props => <Image style={{ width: 150, height: 50 }} source={require('./assets/image/logo.png')} />,
+          // 这种方式声明的button是访问不到HomePage这个页面的instance（实例的）解决方案请看listPage页面
+          headerRight: props => <Button title="按 钮" onPress={() => alert('This is a button!')} color="#fff" />,
+          headerLeft: props => <Button title="left button" onPress={() => alert('left button!')} color="#fff" />
+        }}
+        initialParams={{ name: 'name-props' }}
+      />
+      <Tab.Screen name="My" component={MyPage} />
+    </Tab.Navigator>
+  )
+}
 
 function MainStackScreen() {
   return (
@@ -31,17 +80,6 @@ function MainStackScreen() {
         },
       }}
     >
-      <MainStack.Screen
-        name="Home"
-        component={HomePage}
-        options={{
-          headerTitle: props => <Image style={{ width: 150, height: 50 }} source={require('./assets/image/logo.png')} />,
-          // 这种方式声明的button是访问不到HomePage这个页面的instance（实例的）解决方案请看listPage页面
-          headerRight: props => <Button title="按 钮" onPress={() => alert('This is a button!')} color="#fff" />,
-          headerLeft: props => <Button title="left button" onPress={() => alert('left button!')} color="#fff" />
-        }}
-        initialParams={{ name: 'name-props' }}
-      />
       <MainStack.Screen
         name="Detail"
         component={DetailPage}
@@ -101,14 +139,14 @@ export default class Txclass extends Component {
 
   render() {
     return (
-      // <View style={style.container}>
-      //   <Text style={[style.text, style.redText]}> Txclass1 </Text>
-      //   <Content contentText={this.state.contentText} handleContentClick={this.handleContentClick}/>
-      // </View>
       <NavigationContainer>
         <RootStack.Navigator
           mode="modal"
         >
+          <RootStack.Screen
+            name="Tab"
+            component={TabStackScreen}
+          />
           <RootStack.Screen
             name="Main"
             component={MainStackScreen}
