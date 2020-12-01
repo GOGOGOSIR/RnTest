f5deac556bb63468bd9c1319f174a375c98da2d3
wanjianqiang <17688728066@163.com>
2020-12-01 09:46:41 +0800

    在最外层包裹SafeAreaProvider

--- app/utils/createNavigation/index.js
+++ app/utils/createNavigation/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RootStackScreen} from './create';
 
 const AppNavigation = () => {
   return (
-    // <SafeAreaProvider>
-    <NavigationContainer>{RootStackScreen()}</NavigationContainer>
-    // </SafeAreaProvider>
+    <SafeAreaProvider>
+      <NavigationContainer>{RootStackScreen()}</NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
