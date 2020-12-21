021ef31c3c66e1f8ced493ab51df7e9cd9de14a7
wanjianqiang <17688728066@163.com>
2020-12-21 09:19:46 +0800

    feat: swipeTabPage新增返回按钮

--- app/pages/laboratoryPage/SwipeTabPage/SwipeTabPage.js
+++ app/pages/laboratoryPage/SwipeTabPage/SwipeTabPage.js
@@ -1,13 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {PureComponent} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import CustomSafeAreaView from '../../../components/CustomSafeAreaView/CustomSafeAreaView';
+import Icon from '../../../components/Icon/Icon';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 import CurstomTabBar from './component/CurstomTabBar';
 import VideoList from './component/VideoList';
 import LiveList from './component/LiveList';
 import SearchContainer from './component/SearchContainer';
-
 @CustomSafeAreaView({
   safeAreaViewStyle: {
     backgroundColor: '#F5F5F9',
@@ -17,7 +17,7 @@ class SwipeTabPage extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {};
-    this._renderTabBarFooterComponent();
+    this._goBack = this._goBack.bind(this);
   }
 
   _renderTabBarFooterComponent() {
@@ -28,9 +28,13 @@ class SwipeTabPage extends PureComponent {
     );
   }
 
+  _goBack() {
+    this.props.navigation.goBack();
+  }
+
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, position: 'relative'}}>
         <ScrollableTabView
           renderTabBar={() => (
             <CurstomTabBar
@@ -41,6 +45,9 @@ class SwipeTabPage extends PureComponent {
           <LiveList tabLabel="直播看房" />
           <VideoList tabLabel="视频看房" />
         </ScrollableTabView>
+        <TouchableOpacity style={styles.backBtn} onPress={this._goBack}>
+          <Icon name="arrow_left" size={27} color="#000000" />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -49,6 +56,11 @@ class SwipeTabPage extends PureComponent {
 const styles = StyleSheet.create({
   searchWrapper: {
     paddingVertical: 10,
+  },
+  backBtn: {
+    position: 'absolute',
+    left: 15,
+    top: 8,
   },
 });
 export default SwipeTabPage;
